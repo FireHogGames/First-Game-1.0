4302794c8bd3bde42bbbd4bb5af604aabc422a82
FireHogGames <hidde01@live.nl>
2019-02-10 02:25:06 +0100

    Started prototyping

---
+++ 4_Project/First_Game/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerController controller;
+
+    //Movement Floats
+    private float horizontalMovement = 0f;
+    [SerializeField] private float speed;
+
+    //bools
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMovement, false, isJumping);
+    }
+}
